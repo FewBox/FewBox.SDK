838edc214628103321c65fbe54b4f9e2780b9f2c
landpy <landpy@outlook.com>
2020-11-29 20:48:57 +0800

    Change QueueNames to public.

--- FewBox.SDK/Core/QueueNames.cs
+++ FewBox.SDK/Core/QueueNames.cs
@@ -1,6 +1,6 @@
 namespace FewBox.SDK.Core
 {
-    static class QueueNames
+    public static class QueueNames
     {
         public static string Mail = "fewbox-mail";
         public static string Realtime = "fewbox-realtime";
