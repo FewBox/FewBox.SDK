51ece6de728ffc115b969f6b17b71bca54194539
FewBox <fewbox.com@gmail.com>
2020-12-01 16:02:52 +0800

    Update PlanMessage.

--- FewBox.SDK/Core/PlanMessage.cs
+++ FewBox.SDK/Core/PlanMessage.cs
@@ -2,6 +2,9 @@ namespace FewBox.SDK.Core
 {
     public class PlanMessage
     {
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string WeChat { get; set; }
         public PlanType Type { get; set; }
     }
 }
